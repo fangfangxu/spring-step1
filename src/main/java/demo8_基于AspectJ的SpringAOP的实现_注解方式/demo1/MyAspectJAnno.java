550c1a6af92b2b668fad450d3b464310f18fa41e
fangfangxu <472848022@qq.com>
2020-01-27 20:09:36 +0800

    commit
    commit

--- src/main/java/demo8_基于AspectJ的SpringAOP的实现_注解方式/demo1/MyAspectJAnno.java
+++ src/main/java/demo8_基于AspectJ的SpringAOP的实现_注解方式/demo1/MyAspectJAnno.java
@@ -1,6 +1,7 @@
 package demo8_基于AspectJ的SpringAOP的实现_注解方式.demo1;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -16,5 +17,10 @@ public class MyAspectJAnno {
   @Before(value = "execution(* demo8_基于AspectJ的SpringAOP的实现_注解方式.demo1.ProductDao.save(..))")
   public void before(JoinPoint joinPoint){
       System.out.println("前置通知===================="+joinPoint);
+  }
+
+  @AfterReturning(value = "execution(* demo8_基于AspectJ的SpringAOP的实现_注解方式.demo1.ProductDao.update(..))")
+  public void afterReturning(JoinPoint joinPoint){
+      System.out.println("后置通知===================="+joinPoint);
   }
 }
